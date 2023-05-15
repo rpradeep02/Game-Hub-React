9f776d8711cf6a6b6d178157f008de5925a6c3a8
rpradeep02 <kumarrpradeep123@gmail.com>
2023-05-15 13:35:58 +0530

    fetch the genres

--- Game-hub/src/hooks/useGames.ts
+++ Game-hub/src/hooks/useGames.ts
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from "react"
+import { useEffect, useState } from "react"
 import apiClient from "../services/api-client";
-
+import { CanceledError } from "axios";
 
 export interface Platform {
   id: number;
