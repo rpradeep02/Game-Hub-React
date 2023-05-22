671798230bd4bc904ae7618a14d40cd38906bc63
rpradeep02 <kumarrpradeep123@gmail.com>
2023-05-23 00:26:30 +0530

    added emoji

---
+++ Game-hub/src/components/Emoji.tsx
@@ -0,0 +1,22 @@
+import bullsEye from "../assets/bulls-eye.png";
+import thumbsUp from "../assets/thumbs-up.png";
+import meh from "../assets/meh.png";
+import { Image, ImageProps } from "@chakra-ui/react";
+
+interface Pops {
+  rating: number;
+}
+
+const Emoji = ({ rating }: Pops) => {
+  if (rating < 5) return null;
+
+  const emojiMap: { [key: number]: ImageProps } = {
+    3: { src: meh, alt: "meh" },
+    4: { src: thumbsUp, alt: "recommended" },
+    5: { src: bullsEye, alt: "exceptional" },
+  };
+
+  return <Image {...emojiMap[rating]} boxSize="25px" marginTop={1} />;
+};
+
+export default Emoji;
