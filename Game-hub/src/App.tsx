350c88cee53e6d295ea5f0a4221986fe2d9ddcc5
rpradeep02 <kumarrpradeep123@gmail.com>
2023-05-23 00:47:44 +0530

    build fix

--- Game-hub/src/App.tsx
+++ Game-hub/src/App.tsx
@@ -6,7 +6,7 @@ import { useState } from "react";
 import { Genre } from "./hooks/useGenres";
 import PlatFromSelector from "./components/PlatFromSelector";
 import { Platform } from "./hooks/useGames";
-import SortSelector from "./components/sortSelector";
+import SortSelector from "./components/SortSelector";
 import GameHeading from "./components/GameHeading";
 
 export interface GameQuery {
